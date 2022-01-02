bd6b99cfe3902a859417807172388cbbfdbae4f8
Sharad Agarwal <obviouslysharad@gmail.com>
2022-01-02 23:46:24 +0530

    Initialized the cryptoverse and added routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route } from "react-router-dom";
+import "./App.css";
+import Header from "./components/Header";
+import Homepage from "./components/Homepage";
+import CoinPage from "./components/CoinPage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Header />
+      <Route path="/" components={Homepage} />
+      <Route path="/coins/:id" components={CoinPage} exact />
+    </BrowserRouter>
   );
 }
 
